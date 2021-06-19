1a149d313adfb9c1c90ea65f6e4dc75477b3c16d
Mike <tankf33der@disroot.org>
2021-06-19 14:42:04 +0300

    .

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 mono:
-	gcc -O3 -Wall -Wextra -DED25519_CUSTOMHASH -DED25519_TEST -DED25519_NO_INLINE_ASM -DED25519_FORCE_32BIT -lsodium *.c
+	gcc -O3 -Wall -Wextra *.c
 	./a.out
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-// #include "monocypher.h"
+#include <strings.h>
 #include "monocypher-ed25519.h"
 #include "utils.h"
-#include "ed25519.h"
+#include "edsign.h"
 
 typedef uint8_t u8;
 typedef uint64_t u64;
@@ -12,36 +12,31 @@ typedef uint64_t u64;
     u8 name[size]; \
     for(size_t i = 0; i < size; i++) name[i] = i;
 
+
+void print_vector(const u8 *buf, size_t size)
+{
+    for(size_t i =0; i < size; i++) {
+        printf("%02x", buf[i]);
+    }
+    printf("\n");
+}
+
 int main(void) {
     ARRAY(hash, 64);
-    ARRAY(sk1, 32);
-    ARRAY(pk1, 32);
-    ARRAY(msg1, 64);
 
-    ARRAY(hash2, 64);
-    ARRAY(pk2, 32);
-    ARRAY(sk2, 32);
-    ARRAY(msg2, 64);
+	ARRAY(hash2, 64);
+	ARRAY(msg2, 64);
+	ARRAY(pk2, 32);
 
+	for(size_t i = 0; i < 1000; i++) {
+		crypto_ed25519_sign(hash, hash, 0, hash, 64);
 
-
-
-	for(size_t i = 0; i < 1; i++) {
-		crypto_ed25519_public_key(pk1, sk1);
-		crypto_ed25519_sign(hash, sk1, pk1, msg1, 64);
-
-		ed25519_publickey(sk2, pk2);
-		ed25519_sign(msg2, 64, hash2, pk2, 	hash2);
-
+		edsign_sec_to_pub(pk2, hash2);
+		edsign_sign(hash2, pk2, hash2, msg2, 64);
+		memcpy(msg2, hash2, 64);
 	}
-
-
 	print_vector(hash, 64);
 	print_vector(hash2, 64);
-	print_vector(pk1, 32);
-	print_vector(pk2, 32);
-	print_vector(sk1, 32);
-	print_vector(sk2, 32);
 	return 0;
 }
 
