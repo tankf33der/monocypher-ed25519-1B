71dfbbc26b9b4b32f55d055912eb718d14cc6289
Mike <tankf33der@disroot.org>
2021-06-19 10:56:07 +0300

    .

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include "monocypher.h"
 #include "monocypher-ed25519.h"
-#include "ed25519.h"
 
 typedef uint8_t u8;
 
@@ -21,21 +20,11 @@ void print_vector(const u8 *buf, size_t size)
 
 int main(void) {
     ARRAY(hash, 64);
-    ARRAY(ehash, 64);
-    ARRAY(epub,  32);
 
-	for(size_t i = 0; i < 32; i++) {
-
-		// ed25519_publickey(ehash, epub);
-		// ed25519_sign(ehash, 64, ehash, epub, ehash);
-
+	for(size_t i = 0; i < 100000; i++) {
 		crypto_ed25519_sign(hash, hash, 0, hash, 64);
-
 	}
 	print_vector(hash, 64);
-	print_vector(ehash, 64);
-
-
 	return 0;
 }
 
